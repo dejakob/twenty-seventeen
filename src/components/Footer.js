1f7df86ab68ba8f2fa9a89b3e77781ac79181d14
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2017-12-30 19:11:21 +0100

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,9 +11,22 @@ const Wrapper = styled.footer`
         margin-top: -11vh;
     }
 `;
+const H2 = styled.h2`
+    margin: 32px 16px 64px 16px;
+    font-size: 32px;
+    text-align: center;
+`;
+const P = styled.p`
+    font-size: 28px;
+    text-align: center;
+
+    & + p {
+        margin-top: 24px;
+    }
+`;
 const Small = styled.small`
     display: block;
-    padding: 16px 0;
+    padding: 64px 0 32px 0;
     text-align: center;
     font-size: 12px;
 
@@ -26,7 +39,26 @@ const Small = styled.small`
 function Footer() {
     return (
         <Wrapper>
-            <p></p>
+            <H2>So, this was 2017</H2>
+            <P className="handwriting">
+                Life is like a video game
+            </P>
+            <P className="handwriting">
+                Some levels are fun, <br />
+                Some levels are hard
+            </P>
+            <P className="handwriting">
+                You never know which level is next <br />
+                But you want to play it anyway
+            </P>
+            <P className="handwriting">
+                You can never go back to the previous level <br />
+                And you have to move forward
+            </P>
+            <P className="handwriting">
+                Let's play level 2018 <br />
+                And activate some superpower
+            </P>
             <Small>Special thanks to <a href="https://icons8.com" target="_blank">icons8</a> for some icons</Small>
         </Wrapper>
     );
