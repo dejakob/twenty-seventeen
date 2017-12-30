272fcb4b24a158b52b8b3660f775725ef0de0029
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2017-12-30 12:52:29 +0100

    ImageDivider animation

--- src/components/ImageDivider.js
+++ src/components/ImageDivider.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import styled from 'styled-components';
+import OnVisible from 'react-on-visible';
+import styled, { keyframes } from 'styled-components';
 
+const figureFadeIn = keyframes`
+    from {
+        visibility: hidden;
+        opacity: 0;
+    }
+    to {
+        opacity: 1;
+        visibility: visible;
+    }
+`;
 const Figure = styled.figure`
     background-position: center center;
     background-size: cover;
     height: 30vh;
+    opacity: 0;
+    visibility: hidden;
+
+    .visible & {
+        animation: ${figureFadeIn} 2s;
+        opacity: 1;
+        visibility: visible;
+    }
 
     @media (min-width: 1200px) {
         background-attachment: fixed;
@@ -12,10 +31,16 @@ const Figure = styled.figure`
 `;
 
 function ImageDivider(props) {
+    const FigureWithImage = styled(Figure)`
+        .visible & {
+            background-image: url(${props.imageSource});
+        }
+    `;
+
     return (
-        <Figure
-            style={{ backgroundImage: `url(${props.imageSource})` }}
-        />
+        <OnVisible>
+            <FigureWithImage />
+        </OnVisible>
     );
 }
 
